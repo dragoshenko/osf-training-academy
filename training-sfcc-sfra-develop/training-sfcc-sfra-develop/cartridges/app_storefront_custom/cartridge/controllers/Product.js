777dc19ffaf2df1f13401f5b6906d055f46604b7
Dragos Ionascu <ionascudragos340@yahoo.com>
2025-02-25 20:58:54 +0200

    Follow URL works and Image work

--- training-sfcc-sfra-develop/training-sfcc-sfra-develop/cartridges/app_storefront_custom/cartridge/controllers/Product.js
+++ training-sfcc-sfra-develop/training-sfcc-sfra-develop/cartridges/app_storefront_custom/cartridge/controllers/Product.js
@@ -7,6 +7,12 @@ server.extend(base);
 
 server.append('Show', function (req, res, next) {
     var ProductMgr = require('dw/catalog/ProductMgr');
+<<<<<<< Updated upstream
+=======
+    var URLUtils = require('dw/web/URLUtils');
+    var Logger = require('dw/system/Logger');
+
+>>>>>>> Stashed changes
     var productId = res.getViewData().product.id;
     var product = ProductMgr.getProduct(productId);
     var suggestedProducts = [];
@@ -29,7 +35,31 @@ server.append('Show', function (req, res, next) {
         for (var index = 0; index < 4 && index < productSearch.productIds.length; index++) {
             var suggestedProductId = productSearch.productIds[index].productID;
             var suggestedProduct = ProductMgr.getProduct(suggestedProductId);
+<<<<<<< Updated upstream
             suggestedProducts.push(suggestedProduct);
+=======
+
+            if (suggestedProduct) {
+                // Attempt to retrieve the image
+                var image = suggestedProduct.getImage('medium', 0);
+                var imageURL = image ? image.getURL().toString() : '/path/to/default/image.jpg';
+
+                // Attempt to retrieve the price
+                var price = suggestedProduct.priceModel ? suggestedProduct.priceModel.price.toFormattedString() : 'Price not available';
+
+                var productURL = URLUtils.url('Product-Show', 'pid', suggestedProduct.ID).toString();
+
+                suggestedProducts.push({
+                    name: suggestedProduct.name,
+                    imageURL: imageURL,
+                    price: price,
+                    url: productURL
+                });
+
+                // Log the suggested product details for debugging
+                Logger.info('Suggested Product: {0}', JSON.stringify(suggestedProducts[suggestedProducts.length - 1]));
+            }
+>>>>>>> Stashed changes
         }
     }
 
