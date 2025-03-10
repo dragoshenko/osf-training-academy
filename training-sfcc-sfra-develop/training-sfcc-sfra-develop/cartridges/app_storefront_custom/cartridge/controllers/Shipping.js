f090efca5d457f3449077142993f87641ad1794b
Dragos Ionascu <ionascudragos340@yahoo.com>
2025-03-10 18:25:12 +0200

    Commit final shipping build
    Implemented a shipping build for the website, which also includes Payment informations and a Scroll to Top button.
    Added a custom functionality so that one cannot press the Save button if the Card Number and Security Code are not in the correct template.

--- training-sfcc-sfra-develop/training-sfcc-sfra-develop/cartridges/app_storefront_custom/cartridge/controllers/Shipping.js
+++
@@ -1,8 +0,0 @@
-server.get("EditAddress", function (req, res, next) {
-    var addressForm = server.forms.getForm("address"); 
-    addressForm.clear();
-    
-    res.render("account/editAddress", {
-        addressForm: addressForm
-    });
-});
