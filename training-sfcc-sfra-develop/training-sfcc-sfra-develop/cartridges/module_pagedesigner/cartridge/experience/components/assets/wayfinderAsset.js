c1ec24c85ccb9d42cac908cfc916da968952a189
Dragos Ionascu <ionascudragos340@yahoo.com>
2025-03-22 19:21:13 +0200

    Commit wayfinder changes

--- training-sfcc-sfra-develop/training-sfcc-sfra-develop/cartridges/module_pagedesigner/cartridge/experience/components/assets/wayfinderAsset.js
+++ training-sfcc-sfra-develop/training-sfcc-sfra-develop/cartridges/module_pagedesigner/cartridge/experience/components/assets/wayfinderAsset.js
@@ -1,5 +1,40 @@
 'use strict';
 
-module.exports = {
-    // Define any server-side logic or exports needed for the asset
+var Template = require('dw/util/Template');
+var HashMap = require('dw/util/HashMap');
+var URLUtils = require('dw/web/URLUtils');
+var ImageTransformation = require('~/cartridge/experience/utilities/ImageTransformation.js');
+
+/**
+ * Render logic for the assets.wayfinderAsset.
+ */
+module.exports.render = function (context) {
+    var model = new HashMap();
+    var content = context.content;
+
+    // Transform image for different devices
+    if (content.image) {
+        model.image = {
+            src: {
+                mobile: ImageTransformation.url(content.image, { device: 'mobile' }),
+                desktop: ImageTransformation.url(content.image, { device: 'desktop' })
+            },
+            alt: content.image.file.getAlt()
+        };
+    }
+
+    // Set category URL using URLUtils
+    if (content.category) {
+        model.categoryUrl = URLUtils.url('Search-Show', 'cgid', content.category.ID);
+    } else {
+        model.categoryUrl = URLUtils.url('Home-Show');
+    }
+
+    // Set title
+    if (content.title) {
+        model.title = content.title;
+    }
+
+    // Render the ISML template for the Wayfinder Asset
+    return new Template('experience/components/assets/wayfinderAsset').render(model).text;
 };
