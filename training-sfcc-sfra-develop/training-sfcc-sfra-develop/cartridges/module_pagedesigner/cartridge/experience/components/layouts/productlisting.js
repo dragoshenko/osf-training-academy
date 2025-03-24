af3245b02bc80942151df50ec0761713e6ca941e
Dragos Ionascu <ionascudragos340@yahoo.com>
2025-03-24 18:42:04 +0200

    Update

--- training-sfcc-sfra-develop/training-sfcc-sfra-develop/cartridges/module_pagedesigner/cartridge/experience/components/layouts/productlisting.js
+++ training-sfcc-sfra-develop/training-sfcc-sfra-develop/cartridges/module_pagedesigner/cartridge/experience/components/layouts/productlisting.js
@@ -1,23 +1,39 @@
 'use strict';
 
 var Template = require('dw/util/Template');
-var ProductViewModel = require('*/cartridge/experience/viewmodels/ProductViewModel');
+var HashMap = require('dw/util/HashMap');
+var ProductMgr = require('dw/catalog/ProductMgr');
+var Logger = require('dw/system/Logger');
 
-/**
- * Render logic for the assets.producttile.
- */
 module.exports.render = function (context) {
+    var model = new HashMap();
     var content = context.content;
 
-    var product = content.product;
-    var model = ProductViewModel.get(product);
+    try {
+        var products = [];
 
-    // overload with explicite configs
-    if (content.shop_now_target) {
-        model.url = content.shop_now_target;
+        if (content.category) {
+            var category = ProductMgr.getCategory(content.category);
+            if (category) {
+                products = category.getProducts().toArray().slice(0, 4); // Get up to 4 products from the category
+            }
+        } else if (content.productIds) {
+            var productIds = content.productIds.split(',').map(function (id) { return id.trim(); });
+            products = productIds.map(function (id) {
+                return ProductMgr.getProduct(id);
+            }).filter(function (product) {
+                return product !== null;
+            }).slice(0, 4); // Get up to 4 products by ID
+        }
+
+        if (products.length > 0) {
+            model.products = products;
+        } else {
+            Logger.warn('No products found for the given category or product IDs.');
+        }
+    } catch (error) {
+        Logger.error('Error fetching products: {0}', error.message);
     }
-    if (content.text_headline) {
-        model.text_headline = content.text_headline;
-    }
-    return new Template('experience/components/assets/producttile').render(model).text;
+
+    return new Template('experience/components/layouts/productlisting').render(model).text;
 };
